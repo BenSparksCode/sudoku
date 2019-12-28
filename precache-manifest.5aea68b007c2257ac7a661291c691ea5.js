266589cf73d46bcd193e0087bb27c15b4cc032a6
Ben Sparks <bensparks0@gmail.com>
2019-12-28 20:24:14 +0200

    Updates

---
+++ precache-manifest.5aea68b007c2257ac7a661291c691ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45c437ef32866e51fb02299489097b1",
+    "url": "bensparkscode.github.io/sudoku/index.html"
+  },
+  {
+    "revision": "15e91e01a255dcb1a5df",
+    "url": "bensparkscode.github.io/sudoku/static/css/main.b23e9448.chunk.css"
+  },
+  {
+    "revision": "5d2b159016bf01add8a0",
+    "url": "bensparkscode.github.io/sudoku/static/js/2.a8936914.chunk.js"
+  },
+  {
+    "revision": "a4b9210ebafe42240037bf71cea84c39",
+    "url": "bensparkscode.github.io/sudoku/static/js/2.a8936914.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15e91e01a255dcb1a5df",
+    "url": "bensparkscode.github.io/sudoku/static/js/main.55e79fd9.chunk.js"
+  },
+  {
+    "revision": "1e3585ba7407d15f8fed",
+    "url": "bensparkscode.github.io/sudoku/static/js/runtime-main.d96f475a.js"
+  }
+]);
